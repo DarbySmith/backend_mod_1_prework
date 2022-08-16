1ad3062c8eba101e3596e953e5381a096b936caa
Darby Smith <newman.darby2@gmail.com>
2022-08-16 12:03:11 -0400

    Add answers and code

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -12,16 +12,27 @@ print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  p "#{name}"
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method:
-# YOUR CODE HERE
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
+def num_sum(num1, num2)
+  num1 + num2
+end
+
+p num_sum(45, 68)
+
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
 # Then call your method:
+
+def two_strings(dog, cat)
+  "#{dog.capitalize} is always chasing #{cat.capitalize}"
+end
+
+p two_strings("dug", "donuts")
--- day_4/questions.md
+++ day_4/questions.md
@@ -2,10 +2,24 @@
 
 1. In your own words, what is the purpose of a method?
 
+    * Methods help write code faster. They are shortcuts for things you want to be able to call on later or not have to write the condition for each time.
+
 1. Create a method named `hello` that will print `"Sam I am"`.
-
+```
+def hello(name)
+  "#{name} I am"
+end
+```
 1. Create a method named `hello_someone` that takes an argument of `name` and prints `"#{name} I am"`.
-
+```
+def hello_someone(name)
+  p "#{name} I am"
+end
+```
 1. How would you call or execute the method that you created above?
-
+```
+hello_someone(darby)
+````
 1. What questions do you have about methods in Ruby?
+
+    * I understand methods right now. I want more practice with challenging scenarios to help my understanding.
